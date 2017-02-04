5ca07ae41469e383f5d64a836b0879b92468cce8
tenchanome <tencha-nome@hotmail.co.jp>
2017-02-05 03:14:49 +0900

    .gitconfigをバージョン管理に追加

--- mklink.bat
+++ mklink.bat
@@ -1,5 +1,6 @@
 mkdir %HOMEPATH%"\vimfiles" > NUL 2>&1
 if ERRORLEVEL 1 cmd /c exit 0
+mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
 mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink %HOMEPATH%"\.ideavimrc" %HOMEPATH%"\dotfiles\.ideavimrc"
 mklink %HOMEPATH%"\.vimperatorrc" %HOMEPATH%"\dotfiles\.vimperatorrc"
