7fd11f8cd37e07e10354bb349f5bb1d4049e1976
tenchanome <tehnca-nome@hotmail.co.jp>
2019-06-07 23:51:16 +0900

    Change to use dot

--- install.sh
+++ install.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
-for f in .??*
-do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
+DOT_DIR="$(builtin cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")" && pwd)"
+DOT_REPO="https://github.com/tenchanome/dotfiles.git"
+DOT_SCR="${HOME}/.dot"
 
-    ln -sf ~/dotfiles/"$f" ~/
-done
+echo "-------------------------"
+echo " Install dotfiles "
+echo "-------------------------"
+echo
+echo "First, Clone dot command from GitHub."
+echo "  * git clone https://github.com/ssh0/dot.git ${DOT_SCR}"
+echo
+git clone https://github.com/ssh0/dot.git "${DOT_SCR}"
+
+# Source dot command
+if [ -f "${DOT_SCR}/dot.sh" ] && . "${DOT_SCR}/dot.sh"; then
+    echo "dot is succefully installed."
+    echo
+else
+    echo "dot cannot be installed."
+    echo
+    echo "Try manuall installation."
+    echo
+    exit 1
+fi
+
+# Create temporary configuration file to set some variables.
+temp_config="$(mktemp)"
+cat << EOF > "${temp_config}"
+clone_repository='${DOT_REPO}'
+dotdir='${DOT_DIR}'
+EOF
+
+dot_main -c "${temp_config}" set --force --verbose
+
