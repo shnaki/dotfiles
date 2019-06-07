3868504df4e96dd240fb19b11cecf66ccc8008ae
tenchanome <tehnca-nome@hotmail.co.jp>
2019-06-07 20:24:15 +0900

    Remove vimperattor settings

--- etc/mklink.bat
+++ etc/mklink.bat
@@ -3,8 +3,6 @@ if ERRORLEVEL 1 cmd /c exit 0
 mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
 mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink %HOMEPATH%"\.ideavimrc" %HOMEPATH%"\dotfiles\.ideavimrc"
-mklink %HOMEPATH%"\.vimperatorrc" %HOMEPATH%"\dotfiles\.vimperatorrc"
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.dein.toml" %HOMEPATH%"\dotfiles\.dein.toml"
 mklink %HOMEPATH%"\.deinlazy.toml" %HOMEPATH%"\dotfiles\.deinlazy.toml"
-mklink /d %HOMEPATH%"\vimperator\colors" %HOMEPATH%"\dotfiles\vimperator\colors"
